92c469471c498377c686c4e5a27ba8f8a8b5fec7
Quentin Raynaud <quentin.raynaud@lsystems.fr>
2015-06-12 11:43:48 +0200

    build fixes

--- release/i18next.commonjs.withJQuery-1.9.0.js
+++ release/i18next.commonjs.withJQuery-1.9.0.js
@@ -812,8 +812,12 @@
             if (lng === 'nb-NO' || lng === 'nn-NO' || lng === 'nb-no' || lng === 'nn-no') lng_index = 1;
             return lng_index;
         },
-        toLanguages: function(lng) {
+        toLanguages: function(lng, fallbackLng) {
             var log = this.log;
+    
+            fallbackLng = fallbackLng || o.fallbackLng;
+            if (typeof fallbackLng === 'string')
+                fallbackLng = [fallbackLng];
     
             function applyCase(l) {
                 var ret = l;
@@ -850,8 +854,8 @@
                 addLanguage(applyCase(lng));
             }
     
-            for (var i = 0; i < o.fallbackLng.length; i++) {
-                if (languages.indexOf(o.fallbackLng[i]) === -1 && o.fallbackLng[i]) languages.push(applyCase(o.fallbackLng[i]));
+            for (var i = 0; i < fallbackLng.length; i++) {
+                if (languages.indexOf(fallbackLng[i]) === -1 && fallbackLng[i]) languages.push(applyCase(fallbackLng[i]));
             }
             return languages;
         },
